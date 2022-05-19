1eef4d87d79b238c37aed581eabf94d380819822
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-05-19 13:41:45 +0200

    updated assign term proof

--- proofs2.v
+++ proofs2.v
@@ -17,32 +17,26 @@ Proof.
     inversion H.
 Qed.
 
-(* Goal forall l e σ,
-let s := Assign l e in
-    (exists σ', properTerm s σ σ') \/
-    (exists s' σ', stuck s σ s' σ').
+Goal forall l e σ,
+let p := [Assign l e] in
+    (exists σ', properTerm p σ σ') \/
+    (exists p' σ', stuck p σ p' σ').
 Proof.
   intros x e σ;cbn.
   destruct (R⟦ e ⟧ σ) eqn:He, (L⟦ x ⟧ σ) eqn:Hx;
   destruct σ as [ρ μ]. 
   2-4: 
-    right; exists (Assign x e); exists (ρ,μ);
+    right; exists ([Assign x e]); exists (ρ,μ);
     apply BigStepEnd; intros [c' H];
     inversion H;subst; subst σ0; congruence.
   left.
   eexists. 
-  eapply BigStepTrans.
+  do 2 try eapply BigStepTrans.
   * apply AssignStep;now eassumption.
+  * apply EmptyStep.
   * apply BigStepEnd. apply terminalTerminated.
 Qed.
 
-Goal forall σ,
-    abortion (Abort) σ.
-Proof.
-    intros.
-    eapply BigStepTrans;[constructor|apply BigStepEnd].
-    apply terminalAbortion.
-Qed. *)
 
 
 Section Exercise6_9.
