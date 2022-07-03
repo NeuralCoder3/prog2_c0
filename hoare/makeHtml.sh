2ed224caaad95b47358343eb30984854279d67f6
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-07-03 21:28:33 +0200

    make html for hoare

---
+++ hoare/makeHtml.sh
@@ -0,0 +1,18 @@
+eval $(opam env)
+
+rm -f html/*.*
+
+# alectryon -R . Lib -I . --frontend coq proofs.v
+# alectryon -R . Lib -I . --frontend coq --backend webpage proofs.v -o html/proofs.html
+alectryon -R . Lib -I . --frontend coq --backend webpage *.v
+mv *.html html/
+mv *.js html/
+mv *.css html/
+
+rm -f neuralcoder3.github.io/proofs/while/hoare/*.*
+cp html/* neuralcoder3.github.io/proofs/while/hoare/
+
+cd neuralcoder3.github.io
+git add proofs
+git commit -m "update"
+git push
